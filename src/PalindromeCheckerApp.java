6c941f132d7990e5d64b745779a7be5eb0306017
[200~vendhankalai614-droid <vendhankalai614@gmail.com>
2026-02-13 10:53:33 +0530

    UC1: Application Entry & Welcome Message

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,8 @@
+public class PalindromeCheckerApp {
+    public static void main(String[] args){
+        System.out.println("Welcome to Palindrome Checker Management System ");
+        System.out.println("version : 1.0");
+        System.out.println("System initialized Sucessfully");
+    }
+
+}
